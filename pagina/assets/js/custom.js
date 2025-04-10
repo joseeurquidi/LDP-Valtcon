5c892cba44617509a5db1bc54e4058ce2c6448d2
joseeurquidi <josee.urquidi@gmail.com>
2025-04-10 16:24:42 -0600

    Producto

---
+++ pagina/assets/js/custom.js
@@ -0,0 +1,140 @@
+function scrollToElement(id){
+  let element = document.getElementById(id)
+  element.scrollIntoView({ behavior: "smooth", block: "start", inline: "nearest" });
+}
+(function() {
+  "use strict";
+
+  new Swiper("#swiper1", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination1",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper2", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination2",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper3", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination3",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper4", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination4",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper5", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination5",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper6", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination6",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper7", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination7",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper8", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination8",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper9", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination9",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+  new Swiper("#swiper10", {
+    spaceBetween: 30,
+    loop: true,
+    autoplay: {
+      delay: 2500,
+      disableOnInteraction: false,
+    },
+    pagination: {
+      el: "#swiper-pagination10",
+      dynamicBullets: true,
+      clickable: true,
+    }
+  });
+})()
+
+
